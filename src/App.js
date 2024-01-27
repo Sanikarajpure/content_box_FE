d28b038a6278fd39519e5b6d87774c854671e98d
Sanika Rajpure <sanika.rajpure@gmail.com>
2024-01-27 13:19:51 +0530

    initial app setup, tailwind

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-3xl font-bold underline">Hello world</div>
+    </div>
+  );
+}
+
+export default App;
